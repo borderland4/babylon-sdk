c8d1a678d192251e0e1c889f5c3d9ca469b4a267
Runchao Han <me@runchao.rocks>
2024-05-17 11:46:25 +1000

    further clean up contract / module interaction

--- x/babylon/contract/in_message.go
+++ x/babylon/contract/in_message.go
@@ -1,10 +1,11 @@
 package contract
 
-type (
-	CustomMsg struct {
-		Test *TestMsg `json:"test,omitempty"`
-	}
-	TestMsg struct {
-		Placeholder string `json:"placeholder,omitempty"`
-	}
-)
+// CustomMsg is a message sent from a smart contract to the Babylon module
+// TODO: implement
+type CustomMsg struct {
+	Test *TestMsg `json:"test,omitempty"`
+}
+
+type TestMsg struct {
+	Placeholder string `json:"placeholder,omitempty"`
+}
--- x/babylon/contract/query.go
+++ x/babylon/contract/query.go
@@ -1,15 +1,16 @@
 package contract
 
-// TODO: revise
-type (
-	CustomQuery struct {
-		Test *TestQuery `json:"test,omitempty"`
-	}
-	TestQuery struct {
-		Placeholder string `json:"placeholder,omitempty"`
-	}
-	TestResponse struct {
-		// MaxCap is the max cap limit
-		Placeholder2 string `json:"placeholder2"`
-	}
-)
+// CustomQuery is a query request from a smart contract to the Babylon module
+// TODO: implement
+type CustomQuery struct {
+	Test *TestQuery `json:"test,omitempty"`
+}
+
+type TestQuery struct {
+	Placeholder string `json:"placeholder,omitempty"`
+}
+
+type TestResponse struct {
+	// MaxCap is the max cap limit
+	Placeholder2 string `json:"placeholder2"`
+}
