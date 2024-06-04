ba159a216b971c72a15a7d5b475775c77e33c5cf
Runchao Han <me@runchao.rocks>
2024-06-04 11:32:57 +1000

    update params

--- x/babylon/types/params.go
+++ x/babylon/types/params.go
@@ -1,14 +1,28 @@
 package types
 
+import (
+	"github.com/cometbft/cometbft/libs/rand"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
+)
+
 // DefaultParams returns default babylon parameters
 func DefaultParams(denom string) Params {
-	return Params{ // todo: revisit and set proper defaults
-		MaxGasBeginBlocker: 500_000,
+	return Params{
+		BabylonContractAddress:    sdk.AccAddress(rand.Bytes(address.Len)).String(),
+		BtcStakingContractAddress: sdk.AccAddress(rand.Bytes(address.Len)).String(),
+		MaxGasBeginBlocker:        500_000,
 	}
 }
 
 // ValidateBasic performs basic validation on babylon parameters.
 func (p Params) ValidateBasic() error {
+	if _, err := sdk.AccAddressFromBech32(p.BabylonContractAddress); err != nil {
+		return err
+	}
+	if _, err := sdk.AccAddressFromBech32(p.BtcStakingContractAddress); err != nil {
+		return err
+	}
 	if p.MaxGasBeginBlocker == 0 {
 		return ErrInvalid.Wrap("empty max gas end-blocker setting")
 	}
