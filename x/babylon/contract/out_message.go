8fd6caa7e491310ff115d1c4774088efbdfdb960
Runchao Han <me@runchao.rocks>
2024-06-10 20:58:26 +1000

    pr comments

--- x/babylon/contract/out_message.go
+++ x/babylon/contract/out_message.go
@@ -8,9 +8,9 @@ type SudoMsg struct {
 }
 
 type BeginBlock struct {
-	Height     int64     `json:"height"`       // Height returns the height of the block
-	HashHex    string    `json:"hash_hex"`     // HashHex returns the hash of the block header in hex
-	Time       time.Time `json:"time"`         // Time returns the time of the block
-	ChainID    string    `json:"chain_id"`     // ChainId returns the chain ID of the block
-	AppHashHex string    `json:"app_hash_hex"` // AppHashHex used in the current block header in hex
+	Height     int64     `json:"height"`       // Height is the height of the block
+	HashHex    string    `json:"hash_hex"`     // HashHex is the hash of the block in hex
+	Time       time.Time `json:"time"`         // Time is the time of the block
+	ChainID    string    `json:"chain_id"`     // ChainId is the chain ID of the block
+	AppHashHex string    `json:"app_hash_hex"` // AppHashHex is the app hash of the block in hex
 }
