fc96ac6a9400a71d58d36678f5df70582e6a34db
Mauro Lacy <mauro@lacy.com.es>
2024-06-19 09:59:48 +0200

    Fix: babylon-contract path

--- tests/scripts/copy_local_wasm.sh
+++ tests/scripts/copy_local_wasm.sh
@@ -3,7 +3,7 @@ set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
 CONTRACTS="babylon_contract btc_staking"
-OUTPUT_FOLDER="$(dirname "$0")"
+OUTPUT_FOLDER="$(dirname "$0")/../testdata"
 
 echo "DEV-only: copy from local built instead of downloading"
 
@@ -14,10 +14,10 @@ S=${S:+-$S}
 
 for CONTRACT in $CONTRACTS
 do
-  cp -f  ../babylon-contract/artifacts/"${CONTRACT}${S}".wasm "$OUTPUT_FOLDER/${CONTRACT}.wasm"
+  cp -f  ../../babylon-contract/artifacts/"${CONTRACT}${S}".wasm "$OUTPUT_FOLDER/${CONTRACT}.wasm"
 done
 
-cd ../babylon-contract
+cd ../../babylon-contract
 TAG=$(git rev-parse HEAD)
 cd - 2>/dev/null
 echo "$TAG" >"$OUTPUT_FOLDER/version.txt"
